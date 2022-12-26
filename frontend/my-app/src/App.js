417d5acde0fbb994674c8bbb1b5a0af43f93018c
unknown <mamadoucirecamara@gmail.com>
2022-12-26 21:56:31 +0100

    1.0.2

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,12 +1,14 @@
 import './Styles/App.css';
 import Acceuil from './Component/Acceuil';
-import Rgister from './Component/Users/Register.js'
-
+import Register from './Component/Users/Register.js'
+import Gestion_A_User from './Component/Astuces/Gestion_A_User';
+import Gestion_A_Admin from './Component/Astuces/Gestion_A_Admin';
+import Consult_Astuces from './Component/Astuces/Consult_Astuces';
 
 function  App(){
   return (
     <div className='app'>
-        <Rgister />
+       <Consult_Astuces />
     </div>
      
   );
