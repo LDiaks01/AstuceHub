a3018ca2e912ea1c234b1156f24a3d0399232f12
Tourgi Mustapha <tourgismacbook@MacBook-Air-de-Tourgi.local>
2022-12-26 20:57:46 +0100

    app_astuces

--- backend/app.js
+++ backend/app.js
@@ -7,19 +7,18 @@ const path = require('path');
 app.use('/files', express.static(path.join(__dirname, 'files')));
 
 
-const Users = require("./models/users");
-const Astuces = require("./models/astuces");
-const Commentaires = require("./models/commentaires");
-
 //routes
 const accueil = require('./routes/accueil');
 const register = require('./routes/register');
 const login = require('./routes/login');
 const profil = require('./routes/profil');
+const astuces = require('./routes/astuce');
 app.use('/accueil', accueil);
 app.use('/register', register);
 app.use('/login', login);
 app.use('/profil', profil);
+app.use('/astuces', astuces);
+
 
 /*
 Users.sync().then(()=>{
