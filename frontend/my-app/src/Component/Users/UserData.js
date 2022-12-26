8336caa4b438690b8c955f08df9552638101a7de
TAD <thiernoamadou.diallo@etu.univ-tours.fr>
2022-12-27 00:11:39 +0100

    1.0.4 Gestion Utilisateur frontend

--- frontend/my-app/src/Component/Users/UserData.js
+++
@@ -1,7 +0,0 @@
-const data = [
-    {nom: 'Diallo', prenom: 'Amadou', pseudo: 'Tad', email:'tad@gmail.com', photo:'https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'},
-    {nom: 'Diallo', prenom: 'Thierno', pseudo: 'Tad', email:'tad@gmail.com', photo:'https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'},
-    {nom: 'Diallo', prenom: 'Djondouaa', pseudo: 'Tad', email:'tad@gmail.com', photo:'https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'},
-    {nom: 'Diallo', prenom: 'Djonbarki', pseudo: 'Tad', email:'tad@gmail.com', photo:'https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'},
-]
-export default data;
