eb65611eaf322c1da1781f87733bd53ae5be457e
Lansana Diakite <diakslead@gmail.com>
2022-12-28 00:26:27 +0100

    Mise en place de l'auth via Discord et Google

---
+++ backend/controllers/o_auth_login.js
@@ -0,0 +1,72 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const DiscordStrategy = require('passport-discord').Strategy;
+const jwt = require('jsonwebtoken');
+const Users = require('../models/users');
+const passport = require('passport');
+
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: "275434420653-9epp6flsv4blfms752q6rd9lp09ndrbh.apps.googleusercontent.com",
+        clientSecret: "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",
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        // Vérifiez l'utilisateur en utilisant le profil Google et exécutez le callback
+        console.log("auth reussie via google, p1" + profile.id);
+        Users.findOrCreate({
+            where: { email: profile.emails[0].value },
+            defaults: {
+              nom: profile.name.familyName,
+              prenom : profile.name.givenName,
+              email: profile.emails[0].value,
+              pseudo : profile.emails[0].value,  // le pseudo est également l'email
+              isAdmin : 0,
+              imageUrl: profile.photos[0].value,
+              googleId : profile.id
+            }
+          })
+          .then(([user, created]) => {
+            return cb(null, user);
+          })
+          .catch((err) => {
+            console.error(err);
+            return cb(null,false);
+          })
+
+        
+      }
+    )
+  );
+  
+
+
+  passport.use(new DiscordStrategy({
+    clientID: "1057288807151575111",
+    clientSecret: "dsiGvWrmYzlkRDXwEPyiGHOA0oqZ8r-a",
+    callbackURL: 'http://localhost:3000/auth/discord/callback',
+    scope: ['identify']
+  }, (accessToken, refreshToken, profile, cb) => {
+    // Vérifiez l'utilisateur en utilisant le profil Discord et exécutez le callback
+    const avatarUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.png`;
+    Users.findOrCreate({
+      where: { email: profile.email },
+      defaults: {
+        nom: "null",
+        prenom : "null",
+        email: profile.email,
+        pseudo : profile.username +"#"+profile.discriminator,  // le pseudo est également l'email
+        imageUrl: avatarUrl,
+        discordId : profile.id
+      }
+    })
+    .then(([user, created]) => {
+      return cb(null, user);
+    })
+    .catch((err) => {
+      console.error(err);
+      return cb(null,false);
+    })
+
+    
+  }));
---
+++ backend/routes/o_auth_login.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+require('../controllers/o_auth_login'); //recupération du controlleur pour l'appel
+
+
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+router.get('/google/callback', function(req,res, next) {
+/*Passport fournit une fonction, qui est utilisée comme itinéraire
+ middleware pour authentifier les demandes.authenticate()*/
+    passport.authenticate('google', { failureRedirect: '/login' }, async function(err,user) 
+    {
+        //verifier si l'utilisateur n'existe pas
+        if(err || !user)
+        {
+            console.log(err);
+            res.status(401).send("Bad Crédential / Incorrect User");
+        }
+        //Enfin si l'utilisateur existe
+        if(user)
+        {
+            console.log("auth reussie via google");
+            let token = jwt.sign(
+                {'username' : user.pseudo},
+                'GROUPE_7_JWT_KEY',
+                { expiresIn: '24h'}
+            );
+            //retourner le token de l'utilisateur qui s'est authentifié
+            
+            res.status(200).send({ 
+                token : token
+            });
+        }
+    })(req, res, next);
+    
+});
+
+router.get('/discord', passport.authenticate('discord', { scope: ['identify', 'email'] }));
+
+router.get('/discord/callback', function(req,res, next) {
+    /*Passport fournit une fonction, qui est utilisée comme itinéraire
+     middleware pour authentifier les demandes.authenticate()*/
+        passport.authenticate('discord', { failureRedirect: '/login' }, async function(err,user) 
+        {
+            //verifier si l'utilisateur n'existe pas
+            if(err || !user)
+            {
+                console.log(err);
+                res.status(401).send("Bad Crédential / Incorrect User");
+            }
+            //Enfin si l'utilisateur existe
+            if(user)
+            {
+                console.log("auth reussie via google");
+                let token = jwt.sign(
+                    {'username' : user.pseudo},
+                    'GROUPE_7_JWT_KEY',
+                    { expiresIn: '24h'}
+                );
+                //retourner le token de l'utilisateur qui s'est authentifié
+                
+                res.status(200).send({ 
+                    token : token
+                });
+            }
+        }
+        )(req, res, next);
+        
+    });
+    
+
+
+
+
+module.exports = router;
