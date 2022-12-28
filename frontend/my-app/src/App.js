8a8ac593e9ed0b66edaba252f7cecb527b662621
Lansana Diakite <diakslead@gmail.com>
2022-12-28 22:20:55 +0100

    Mise à jour app.js

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -7,6 +7,9 @@ import Register from './Component/Users/Register.js'
 import Gestion_A_User from './Component/Astuces/Gestion_A_User';
 import Gestion_A_Admin from './Component/Astuces/Gestion_A_Admin';
 import Consult_Astuces from './Component/Astuces/Consult_Astuces';
+import EditUser from './Component/Users/EditUser';
+import ListUsers from './Component/Users/ListUsers';
+import {BrowserRouter as Router,Switch, Routes,Route} from "react-router-dom"
 
 function  App(){
   return (
@@ -21,9 +24,8 @@ function  App(){
        </Routes>
       </Router>
     </div>
-     
+
   );
 }
 
 export default App;
- 
